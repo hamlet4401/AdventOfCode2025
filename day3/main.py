3d1ebe160addea8493098b86f9502b99a03cb641
Hamlet Marekhashvili <hamlet@brightbook.io>
2025-12-03 14:06:22 +0100

    chore: day 3 cleanup

--- day3/main.py
+++ day3/main.py
@@ -4,27 +4,29 @@ def run():
     with open(file_path, 'r') as file:
         for line in file:
             bank = line.rstrip()
-            total_joltage += find_max_joltage_from_bank(bank, 11)
+            total_joltage += find_max_joltage_from_bank(bank, 12)
 
     print(total_joltage)
 
-def find_max_joltage_from_bank(bank, battery_amount):
+def find_max_joltage_from_bank(bank, battery_target):
+    battery_target_index = battery_target - 1
+
     max_v = 0
     index = 0
 
-    for i in range(len(bank) - battery_amount):
+    for i in range(len(bank) - battery_target_index):
         int_v = int(bank[i])
         if int_v > max_v:
             max_v = int_v
             index = i
 
     other_value = (
-        find_max_joltage_from_bank(bank[index + 1:], battery_amount - 1)
-        if battery_amount > 0
+        find_max_joltage_from_bank(bank[index + 1:], battery_target_index)
+        if battery_target_index > 0
         else 0
     )
 
-    return (max_v * 10**battery_amount) + other_value
+    return (max_v * 10**battery_target_index) + other_value
 
 if __name__ == "__main__":
     run()
